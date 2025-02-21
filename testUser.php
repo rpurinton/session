cb98a224c061b5237a8ffcea6dac0b3364d257e4
Russell Purinton <russell.purinton@icloud.com>
2025-02-21 18:04:26 +0000

    Version 1.0.9 - 2025-02-21

--- testUser.php
+++ testUser.php
@@ -1,10 +1,11 @@
+#!/usr/bin/env php
 <?php
 
 use RPurinton\{MySQL, User};
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$sql = new MySQL;
+$sql = MySQL::connect();
 $username = 'rpurinton';
 $password_hash = '5772527c5398c3b1d9999c5a9388823c454126d8a387a32ce461ad7cfc13f656';
 $user = User::authenticate($sql, $username, $password_hash);
