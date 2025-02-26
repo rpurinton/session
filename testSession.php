34ddaa4288146fcefbf3b32280bf9f7c1927beda
Russell Purinton <russell.purinton@icloud.com>
2025-02-26 12:49:14 +0000

    Version 1.0.18 - 2025-02-26

--- testSession.php
+++ testSession.php
@@ -5,9 +5,6 @@ use RPurinton\{Log, MySQL, Session, User};
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-putenv('LOG_LEVEL=trace');
-putenv('LOG_FILE');
-Log::init();
 $session = new Session("example.com", true, true);
 echo "Session Data: " . print_r($session);
 
