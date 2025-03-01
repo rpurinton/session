73b853a6883f5416bb8db340b03836f67383a3d3
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2025-03-01 13:03:26 -0500

    Updates

--- testSession.php
+++ testSession.php
@@ -4,6 +4,6 @@
 use RPurinton\{Log, MySQL, Session, User};
 
 require_once __DIR__ . '/vendor/autoload.php';
-$session = new Session("example.com", true, true);
+Log::install();
+$session = Session::connect(true, true);
 echo "Session Data: " . print_r($session);
-
